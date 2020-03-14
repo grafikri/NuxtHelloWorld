98264f7395cb1f559a36f532c5e8fc35f242428d
grafikri <grafikri@gmail.com>
2020-03-14 23:41:20 +0300

    template added for vuex

---
+++ _templates/vuex/new/prompt.js
@@ -0,0 +1,15 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+//
+module.exports = [
+  {
+    type: 'input',
+    name: 'directory',
+    message: "What's the directory?"
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's store name?"
+  }
+]
